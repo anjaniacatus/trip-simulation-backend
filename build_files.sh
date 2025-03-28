85258cf8a24ec9cca6cb570a06c640be2fdfeb50
afenoum <anja1catus@gmail.com>
2025-03-28 14:55:30 +0300

    fix package to install

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
 python3 -m venv .env_trip
-source .env_trip/bin/activate
+source .trip_env/bin/activate
 pip install -r requirements.txt
 python3 manage.py collectstatic --no-input --clear
