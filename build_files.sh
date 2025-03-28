d7aaff67fadf052961f9b0bf65ab0a6c4743c79a
afenoum <anja1catus@gmail.com>
2025-03-28 15:27:58 +0300

    .

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 -m venv .trip_env
-source .tripenv/bin/activate
+source .trip_env/bin/activate
 pip install -r requirements.txt
 python3 manage.py collectstatic --no-input --clear
