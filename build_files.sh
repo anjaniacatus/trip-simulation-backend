31ec80be1eb9224a1094177bdc17e8355eaf0856
afenoum <anja1catus@gmail.com>
2025-03-28 15:08:25 +0300

    typo

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
-python3 -m venv .trip_env
-source .trip_env/bin/activate
+python3 -m venv env_trip
+source env_trip/bin/activate
 pip install -r requirements.txt
 python3 manage.py collectstatic --no-input --clear
